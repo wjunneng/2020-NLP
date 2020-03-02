1e6eba2058d327926c3d18ec9897203bc2fc6ec0
wwwjjjnnn <1194348056@qq.com>
2020-03-02 11:43:18 +0800

    update tokenization.py.

--- src/bert/tokenization.py
+++ src/bert/tokenization.py
@@ -33,6 +33,7 @@ tokenization.py是对原始文本语料的处理，分为BasicTokenizer和Wordpi
 """
 
 
+# 检查机壳配置是否与检查点名称一致。
 def validate_case_matches_checkpoint(do_lower_case, init_checkpoint):
     """Checks whether the casing config is consistent with the checkpoint name."""
 
@@ -83,6 +84,7 @@ def validate_case_matches_checkpoint(do_lower_case, init_checkpoint):
                                               model_name, case_name, opposite_flag))
 
 
+# 转换编码格式
 def convert_to_unicode(text):
     """Converts `text` to Unicode (if it's not already), assuming utf-8 input."""
     if six.PY3:
@@ -103,6 +105,7 @@ def convert_to_unicode(text):
         raise ValueError("Not running on Python2 or Python 3?")
 
 
+# 打印文本
 def printable_text(text):
     """Returns text encoded in a way suitable for print or `tf.logging`."""
 
@@ -126,6 +129,7 @@ def printable_text(text):
         raise ValueError("Not running on Python2 or Python 3?")
 
 
+# 加载字典
 def load_vocab(vocab_file):
     """Loads a vocabulary file into a dictionary."""
     vocab = collections.OrderedDict()
@@ -141,6 +145,7 @@ def load_vocab(vocab_file):
     return vocab
 
 
+# 根据字典进行转换
 def convert_by_vocab(vocab, items):
     """Converts a sequence of [tokens|ids] using the vocab."""
     output = []
@@ -149,14 +154,17 @@ def convert_by_vocab(vocab, items):
     return output
 
 
+# token -> ids
 def convert_tokens_to_ids(vocab, tokens):
     return convert_by_vocab(vocab, tokens)
 
 
+# ids -> token
 def convert_ids_to_tokens(inv_vocab, ids):
     return convert_by_vocab(inv_vocab, ids)
 
 
+# 按照空格进行切割
 def whitespace_tokenize(text):
     """Runs basic whitespace cleaning and splitting on a piece of text."""
     text = text.strip()
@@ -166,6 +174,7 @@ def whitespace_tokenize(text):
     return tokens
 
 
+# BERT分词的主要接口,包含了上述两种实现。
 class FullTokenizer(object):
     """Runs end-to-end tokenziation."""
 
@@ -204,6 +213,7 @@ class BasicTokenizer(object):
         """
         self.do_lower_case = do_lower_case
 
+    # 格式化一条信息
     def tokenize(self, text):
         """Tokenizes a piece of text."""
         text = convert_to_unicode(text)
@@ -228,17 +238,21 @@ class BasicTokenizer(object):
         output_tokens = whitespace_tokenize(" ".join(split_tokens))
         return output_tokens
 
+    # 从一段文字中去除重音
     def _run_strip_accents(self, text):
         """Strips accents from a piece of text."""
         text = unicodedata.normalize("NFD", text)
         output = []
         for char in text:
             cat = unicodedata.category(char)
+            # Nonspacing mask
             if cat == "Mn":
                 continue
             output.append(char)
+
         return "".join(output)
 
+    # 用标点切分，返回list
     def _run_split_on_punc(self, text):
         """Splits punctuation on a piece of text."""
         chars = list(text)
@@ -259,6 +273,7 @@ class BasicTokenizer(object):
 
         return ["".join(x) for x in output]
 
+    # 如果遇到中文字符,则改字符两侧添加空格
     def _tokenize_chinese_chars(self, text):
         """Adds whitespace around any CJK character."""
         output = []
@@ -272,6 +287,7 @@ class BasicTokenizer(object):
                 output.append(char)
         return "".join(output)
 
+    #  判断是否是中文字符
     def _is_chinese_char(self, cp):
         """Checks whether CP is the codepoint of a CJK character."""
         # This defines a "chinese character" as anything in the CJK Unicode block:
@@ -294,6 +310,7 @@ class BasicTokenizer(object):
 
         return False
 
+    # 去除无意义字符以及空格
     def _clean_text(self, text):
         """Performs invalid character removal and whitespace cleanup on text."""
         output = []
@@ -308,6 +325,9 @@ class BasicTokenizer(object):
         return "".join(output)
 
 
+# WordpieceTokenizer的作用是把词再切分成更细粒度的WordPiece。关于WordPiece(Byte Pair Encoding)我们之前在机器翻译部分已经介绍过了，
+# 它是一种解决OOV问题的方法，如果不管细节，我们把它看成比词更小的基本单位就行。对于中文来说，WordpieceTokenizer什么也不干，
+# 因为之前的分词已经是基于字符的了。有兴趣的读者可以参考这个开源项目。一般情况我们不需要自己重新生成WordPiece，使用BERT模型里自带的就行。
 class WordpieceTokenizer(object):
     """Runs WordPiece tokenziation."""
 
@@ -316,6 +336,7 @@ class WordpieceTokenizer(object):
         self.unk_token = unk_token
         self.max_input_chars_per_word = max_input_chars_per_word
 
+    # 使用贪心的最大正向匹配算法
     def tokenize(self, text):
         """Tokenizes a piece of text into its word pieces.
 
@@ -332,6 +353,12 @@ class WordpieceTokenizer(object):
 
         Returns:
           A list of wordpiece tokens.
+
+        我们用一个例子来看代码的执行过程。
+        比如假设输入是”unaffable”。我们跳到while循环部分，这是start=0，end=len(chars)=9，也就是先看看unaffable在不在词典里，
+        如果在，那么直接作为一个WordPiece，如果不再，那么end-=1，也就是看unaffabl在不在词典里，最终发现”un”在词典里，把un加到结果里。
+        接着start=2，看affable在不在，不在再看affabl，…，最后发现 ##aff 在词典里。
+        注意：##表示这个词是接着前面的，这样使得WordPiece切分是可逆的——我们可以恢复出“真正”的词。
         """
 
         text = convert_to_unicode(text)
@@ -370,6 +397,7 @@ class WordpieceTokenizer(object):
         return output_tokens
 
 
+# 判断某个字符是否是空格
 def _is_whitespace(char):
     """Checks whether `chars` is a whitespace character."""
     # \t, \n, and \r are technically contorl characters but we treat them
@@ -377,11 +405,13 @@ def _is_whitespace(char):
     if char == " " or char == "\t" or char == "\n" or char == "\r":
         return True
     cat = unicodedata.category(char)
+    # Zs:space separator
     if cat == "Zs":
         return True
     return False
 
 
+# 检查字符是否是控制字符
 def _is_control(char):
     """Checks whether `chars` is a control character."""
     # These are technically control characters but we count them as whitespace
@@ -389,11 +419,13 @@ def _is_control(char):
     if char == "\t" or char == "\n" or char == "\r":
         return False
     cat = unicodedata.category(char)
+    # Cc: Control  Cf: Format
     if cat in ("Cc", "Cf"):
         return True
     return False
 
 
+# 判断字符是否是标点符号
 def _is_punctuation(char):
     """Checks whether `chars` is a punctuation character."""
     cp = ord(char)
