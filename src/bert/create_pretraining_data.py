5ab073c3ea1be3cb48b6c1ff4dd85467c981c95f
wwwjjjnnn <1194348056@qq.com>
2020-03-03 17:32:21 +0800

    update create_pretraining_data.py.

--- src/bert/create_pretraining_data.py
+++ src/bert/create_pretraining_data.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Create masked LM/next sentence masked_lm TF examples for BERT."""
-
+# 这个文件的作用就是将原始输入语料转换成模型预训练所需要的数据格式TFRecoed。
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -48,25 +48,40 @@ flags.DEFINE_bool(
 
 flags.DEFINE_integer("max_seq_length", 128, "Maximum sequence length.")
 
+# max_predictions_per_seq： 一个句子里最多有多少个[MASK]标记
 flags.DEFINE_integer("max_predictions_per_seq", 20,
                      "Maximum number of masked LM predictions per sequence.")
 
 flags.DEFINE_integer("random_seed", 12345, "Random seed for data generation.")
 
+# dupe_factor： 重复参数，即对于同一个句子，我们可以设置不同位置的【MASK】次数。
+# 比如对于句子Hello world, this is bert.，为了充分利用数据，第一次可以mask成Hello [MASK], this is bert.，
+# 第二次可以变成Hello world, this is [MASK[.
 flags.DEFINE_integer(
     "dupe_factor", 10,
     "Number of times to duplicate the input data (with different masks).")
 
+# masked_lm_prob： 多少比例的Token被MASK掉
 flags.DEFINE_float("masked_lm_prob", 0.15, "Masked LM probability.")
 
+# short_seq_prob： 长度小于“max_seq_length”的样本比例。
+# 因为在fine-tune过程里面输入的target_seq_length是可变的（小于等于max_seq_length），
+# 那么为了防止过拟合也需要在pre-train的过程当中构造一些短的样本。
 flags.DEFINE_float(
     "short_seq_prob", 0.1,
     "Probability of creating sequences which are shorter than the "
     "maximum length.")
 
 
+# 训练样本的类
 class TrainingInstance(object):
-    """A single training instance (sentence pair)."""
+    """
+    A single training instance (sentence pair).
+
+    __repr__和__str__这两个方法都是用于显示的，__str__是面向用户的，而__repr__面向程序员。
+    __repr__:不管直接输出对象还是使用print打印对象，输出的格式都是按照__repr的来。
+    __str__:只用使用print打印对象的是否才使用__str__定义的格式。
+    """
 
     def __init__(self, tokens, segment_ids, masked_lm_positions, masked_lm_labels,
                  is_random_next):
@@ -176,10 +191,13 @@ def create_float_feature(values):
     return feature
 
 
+# 创建训练实例
 def create_training_instances(input_files, tokenizer, max_seq_length,
                               dupe_factor, short_seq_prob, masked_lm_prob,
                               max_predictions_per_seq, rng):
     """Create `TrainingInstance`s from raw text."""
+    # all_documents是list的list，第一层list表示document，
+    # 第二层list表示document里的多个句子。
     all_documents = [[]]
 
     # Input file format:
@@ -188,6 +206,13 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
     # sentence boundaries for the "next sentence prediction" task).
     # (2) Blank lines between documents. Document boundaries are needed so
     # that the "next sentence prediction" task doesn't span between documents.
+    # 在源码包中Google提供了一个实例训练样本输入（「sample_text.txt」），输入文件格式为：
+    #
+    # 每行一个句子，这应该是实际的句子，不应该是整个段落或者段落的随机片段(span)，
+    # 因为我们需要使用句子边界来做下一个句子的预测。
+    # 不同文档之间用一个空行分割。
+    # 我们认为同一文档的句子之间是有关系的，不同文档句子之间没有关系。
+
     for input_file in input_files:
         with tf.gfile.GFile(input_file, "r") as reader:
             while True:
@@ -197,6 +222,7 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
                 line = line.strip()
 
                 # Empty lines are used as document delimiters
+                # 空行表示文档分割
                 if not line:
                     all_documents.append([])
                 tokens = tokenizer.tokenize(line)
@@ -204,11 +230,13 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
                     all_documents[-1].append(tokens)
 
     # Remove empty documents
+    # 删除空文档
     all_documents = [x for x in all_documents if x]
     rng.shuffle(all_documents)
 
     vocab_words = list(tokenizer.vocab.keys())
     instances = []
+    # dupe_factor 重复设置mask的次数 重复dupe_factor次
     for _ in range(dupe_factor):
         for document_index in range(len(all_documents)):
             instances.extend(
@@ -220,6 +248,7 @@ def create_training_instances(input_files, tokenizer, max_seq_length,
     return instances
 
 
+# 实现从一个文档中抽取多个训练实例
 def create_instances_from_document(
         all_documents, document_index, max_seq_length, short_seq_prob,
         masked_lm_prob, max_predictions_per_seq, vocab_words, rng):
@@ -227,6 +256,7 @@ def create_instances_from_document(
     document = all_documents[document_index]
 
     # Account for [CLS], [SEP], [SEP]
+    # 为[CLS], [SEP], [SEP]预留三个空位
     max_num_tokens = max_seq_length - 3
 
     # We *usually* want to fill up the entire sequence since we are padding
@@ -237,6 +267,8 @@ def create_instances_from_document(
     # The `target_seq_length` is just a rough target however, whereas
     # `max_seq_length` is a hard limit.
     target_seq_length = max_num_tokens
+    # short_seq_prob： 长度小于“max_seq_length”的样本比例
+    # 以short_seq_prob的概率随机生成（2~max_num_tokens）的长度
     if rng.random() < short_seq_prob:
         target_seq_length = rng.randint(2, max_num_tokens)
 
@@ -250,14 +282,18 @@ def create_instances_from_document(
     current_length = 0
     i = 0
     while i < len(document):
+        # segment: 当前句子
         segment = document[i]
         current_chunk.append(segment)
         current_length += len(segment)
+        # 将句子加入到current_chunk直到加完，或者达到限制长度
         if i == len(document) - 1 or current_length >= target_seq_length:
             if current_chunk:
                 # `a_end` is how many segments from `current_chunk` go into the `A`
                 # (first) sentence.
+                # a_end 是第一个句子A结束的下标
                 a_end = 1
+                # 随机选取切分边界
                 if len(current_chunk) >= 2:
                     a_end = rng.randint(1, len(current_chunk) - 1)
 
@@ -434,6 +470,14 @@ def truncate_seq_pair(tokens_a, tokens_b, max_num_tokens, rng):
 
 
 def main(_):
+    """
+    1、构造tokenizer对输入语料进行分词处理（Tokenizer部分会在后续说明）
+    2、经过create_training_instances函数构造训练instance
+    3、调用write_instance_to_example_files函数以TFRecord格式保存数据
+    下面我们一一解析这些函数。
+    :param _:
+    :return:
+    """
     tf.logging.set_verbosity(tf.logging.INFO)
 
     tokenizer = tokenization.FullTokenizer(
@@ -441,6 +485,8 @@ def main(_):
 
     input_files = []
     for input_pattern in FLAGS.input_file.split(","):
+        # tf.gfile.Glob: 查找匹配pattern的文件并以列表的形式返回，
+        # filename可以是一个具体的文件名，也可以是包含通配符的正则表达式。
         input_files.extend(tf.gfile.Glob(input_pattern))
 
     tf.logging.info("*** Reading from input files ***")
